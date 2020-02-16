ab2a79edc79248116a99f84fca0060d0d49ba1b6
Vadim Guseynov <hgt166782@gmail.com>
2020-02-17 02:47:28 +0300

    Release

---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace OnTopper
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            listBoxProcesses.DisplayMember = "ProcessName";
+            update();
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        const uint SWP_NOSIZE = 0x0001;
+        const uint SWP_NOMOVE = 0x0002;
+        const uint SWP_SHOWWINDOW = 0x0040;
+
+        private enum WINDOW_STATE { TOP, UNTOP }
+
+        private void ButtonUpdate_Click(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void update()
+        {
+            listBoxProcesses.Items.Clear();
+            foreach (var process in Process.GetProcesses())
+            {
+                listBoxProcesses.Items.Add(process);
+            }
+        }
+
+        private void ButtonSetTop_Click(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedItem == null)
+            {
+                labelSelectProcess.Show();
+                return;
+            }
+            labelSelectProcess.Hide();
+            setWindowState(WINDOW_STATE.TOP);
+        }
+
+        private void ButtonUnsetTop_Click(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedItem == null)
+            {
+                labelSelectProcess.Show();
+                return;
+            }
+            labelSelectProcess.Hide();
+            setWindowState(WINDOW_STATE.UNTOP);
+        }
+
+        private void setWindowState(WINDOW_STATE state)
+        {
+            try
+            {
+                if (state == WINDOW_STATE.TOP)
+                {
+                    SetWindowPos(getWindowHandle((Process)listBoxProcesses.SelectedItem)
+                    , HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+                }
+                else
+                {
+                    SetWindowPos(getWindowHandle((Process)listBoxProcesses.SelectedItem)
+                    , HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+                }
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Process does not exists anymore",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                update();
+            }
+        }
+
+        private IntPtr getWindowHandle(Process p)
+        {
+            return Process.GetProcessById(p.Id).MainWindowHandle;
+        }
+
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            int id = listBoxProcesses.FindString(textBoxSearch.Text);
+            if (id != -1)
+            {
+                listBoxProcesses.SelectedIndex = id;
+            }
+        }
+
+        private void ButtonThisOnTop_Click(object sender, EventArgs e)
+        {
+            if (TopMost)
+            {
+                buttonThisOnTop.Text = "Set this";
+                TopMost = false;
+            }
+            else
+            {
+                buttonThisOnTop.Text = "Unset this";
+                TopMost = true;
+            }
+        }
+
+        private void ButtonAbout_Click(object sender, EventArgs e)
+        {
+            AboutForm about = new AboutForm();
+            about.ShowDialog();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("OnTopper")]
+[assembly: AssemblyDescription("I can set windows on top!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DeMmAge Inc.")]
+[assembly: AssemblyProduct("OnTopper")]
+[assembly: AssemblyCopyright("Copyright DeMmAge ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("74b06911-1226-4ed0-a6d5-bc241c1aff47")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
