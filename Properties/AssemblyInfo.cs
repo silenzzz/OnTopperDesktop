6240e9ac69f8a75625f0ca9353016c79a1e04f67
Vadim Guseynov <hgt166782@gmail.com>
2020-07-08 02:04:09 +0300

    Forgot to change version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.3")]
-[assembly: AssemblyFileVersion("1.6.0.3")]
+[assembly: AssemblyVersion("1.6.0.4")]
+[assembly: AssemblyFileVersion("1.6.0.4")]
 [assembly: NeutralResourcesLanguage("en")]
 
